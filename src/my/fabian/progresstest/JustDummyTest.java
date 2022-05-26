4acdf74180f76711418064adf1e5e9ec18d337e8
M Fabian <fabian@chalmers.se>
2022-05-26 19:14:32 +0200

    ADD. New branch named NewBranch, and push to repo

---
+++ src/my/fabian/progresstest/JustDummyTest.java
@@ -0,0 +1,5 @@
+// Just for testing how to push local branch to new remote branch
+// This file is only in the "NewBranch" branch created locally
+// This is discussed here
+// https://stackoverflow.com/questions/2765421/how-do-i-push-a-new-local-branch-to-a-remote-git-repository-and-track-it-too
+// Will try: git push -u origin <branch>
